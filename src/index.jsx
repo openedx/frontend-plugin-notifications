1a47c1d60482d16fc6f63be1bc0c15d811b9e93a
Awais Ansari <awais.ansari63@gmail.com>
2025-02-24 18:55:22 +0500

    feat: added notifications tray plugin

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+import Notifications from './Notification';
+import { useAppNotifications, useNotification } from './Notification/data/hook';
+
+export { Notifications, useAppNotifications, useNotification };
