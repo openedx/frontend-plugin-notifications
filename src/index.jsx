e65fe38fe6021bba756cbcac7f42f0be7e25f249
Muhammad Adeel Tajamul <77053848+muhammadadeeltajamul@users.noreply.github.com>
2025-03-05 10:05:29 +0500

    feat: added NotificationsTray component (#10)
    * feat: added NotificationsTray component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,13 @@
-/* eslint-disable import/prefer-default-export */
+import React from 'react';
 import Notifications from './Notification';
 import { useAppNotifications, useNotification } from './Notification/data/hook';
 
+export const NotificationsTray = () => {
+  const { notificationAppData } = useAppNotifications();
+  return notificationAppData?.showNotificationsTray
+    ? <Notifications notificationAppData={notificationAppData} />
+    : '';
+};
+
+export default NotificationsTray;
 export { Notifications, useAppNotifications, useNotification };
