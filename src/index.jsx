7e78f9ab236c9fe2ed3fa1159592cf888948e592
muhammadadeeltajamul <muhammadadeeltajamul@hotmail.com>
2025-03-05 09:27:41 +0500

    feat: added NotificationsTray component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
-/* eslint-disable import/prefer-default-export */
 import Notifications from './Notification';
 import { useAppNotifications, useNotification } from './Notification/data/hook';
 
+export const NotificationsTray = () => {
+  const { notificationAppData } = useAppNotifications();
+  return notificationAppData?.showNotificationsTray
+    ? <Notifications notificationAppData={notificationAppData} />
+    : '';
+};
+
+export default NotificationsTray;
 export { Notifications, useAppNotifications, useNotification };
